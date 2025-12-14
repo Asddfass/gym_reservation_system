34212457e9c82fb4e44851cb3402f32537d35864
Asddfass <alihassansali@gmail.com>
2025-12-14 22:07:22 +0800

    typo

--- admin/admin_reserve.php
+++ admin/admin_reserve.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include '../includes/Database.php';
+include '../includes/database.php';
 include '../includes/functions.php';
 
 if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin') {
--- admin/notifications.php
+++ admin/notifications.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include '../includes/Database.php';
+include '../includes/database.php';
 include '../includes/functions.php';
 include '../includes/NotificationManager.php';
 
